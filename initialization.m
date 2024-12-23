c0f0100a24a8aaedc105a6e87ee9b643b14448dc
İsmail Yücel <127029709+yucelismail@users.noreply.github.com>
2024-12-23 14:46:26 +0300

    Update initialization.m

--- initialization.m
+++ initialization.m
@@ -1,15 +1,15 @@
-% This function initialize the first population of search agents
+% Bu fonksiyon, arama aracılarının ilk popülasyonunu başlatır
 function Positions=initialization(SearchAgents_no,dim,ub,lb)
 
 Boundary_no= size(ub,2); % numnber of boundaries
 
-% If the boundaries of all variables are equal and user enter a signle
-% number for both ub and lb
+% Tüm değişkenlerin sınırları eşitse ve kullanıcı hem ub hem de lb için tek bir
+% sayısı girerse
 if Boundary_no==1
     Positions=rand(SearchAgents_no,dim).*(ub-lb)+lb;
 end
 
-% If each variable has a different lb and ub
+% Her değişkenin farklı bir lb ve ub'si varsa
 if Boundary_no>1
     for i=1:dim
         ub_i=ub(i);
